5ac7533094ce45968b57e2fe77f275527de54494
larispppp <114947336+larispppp@users.noreply.github.com>
2023-03-10 13:56:52 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var canvas = document.getElementById("myCanvas");
 console.log(canvas);
 var ctx = canvas.getContext("2d");
 
-const map1 = ["..#############################",
+let map1 = ["M.#############################",
     "....#...#.....#.......#.#...#.#",
     "#.###.###.###.#.#####.#.###.#.#",
     "#.........#.#.......#...#.#...#",
@@ -22,40 +22,58 @@ const map1 = ["..#############################",
     "#.#.#.......#.#.#.......#...#.#",
     "#.#.#.###.#######.#########.###",
     "#...#.#.......................",
-    "#############################."
+    "#############################.E"
 ];
-const map2 = ["..###################",
-    "..#.#.#.#...#...#...#",
-    "#.#.#.#.###.#.#.#.#.#",
-    "#.#.#...#...#.#...#.#",
-    "#.#.#.###.###.#.#.###",
-    "#.#.#...#...#.#.#...#",
-    "#.#.#.#.###.#.#####.#",
-    "#.....#.........#...#",
-    "#.#####.#.###.#####.#",
-    "#.....#.#.#.....#...",
-    "###################."
-];
+
+
+
 
 drawLab();
 
+document.addEventListener("keypress",(e) => {
+    const key = e.key;
+    switch (key) {
+        case 's':
+            console.log("tine lize kurce");
+            break;
+    
+        default:
+            break;
+    }
+});
+
+
+//maze generator!!
 function drawLab(){
     for(let i=0; i<map1.length; i++){
         let x=0;
         let y=i*20;
         for(let j=0; j<map1[i].length; j++){
             if(map1[i][j]=="#"){
+                ctx.fillStyle='black';
                 ctx.beginPath();
                 ctx.rect(x, y, 20, 20);
-                ctx.stroke();
                 ctx.fill();
                 x=x+20
                 
             }
+            else if(map1[i][j]=="E"){
+                ctx.beginPath();
+                ctx.fillStyle='green';
+                ctx.rect(x, y, 20, 20);
+                ctx.fill();
+                x=x+20 
+            }
+            else if(map1[i][j]=="M"){
+                ctx.beginPath();
+                ctx.fillStyle='red';
+                ctx.rect(x, y, 20, 20);
+                ctx.fill();
+                x=x+20   
+            }
             else{
                 x=x+20
             }
-        }
-        
+        } 
     }
 }
